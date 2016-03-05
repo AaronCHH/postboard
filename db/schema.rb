126a7d3f2e6637ed5dfd3f960d7a4c8c09cff549
AaronCHH <AaronHsu219@gmail.com>
2016-03-05 14:37:34 +0800

    added bycrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,15 @@ ActiveRecord::Schema.define(version: 20160220020620) do
     t.string  "title"
     t.text    "content"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"    
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"    
   end
 
 end
