90e5f44152aa171cea88e2638bda266f612cefc1
AaronCHH <AaronHsu219@gmail.com>
2016-03-05 14:05:37 +0800

    added navigation bar html partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    post = Post.new(post_params)
+    if post.save!
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  
+  # Ensure security
+  private   
+  def post_params
+#    binding.pry
+    params.require(:post).permit(:title, :content)
+  end
+  
+#  strong parameter
+#  mass-update
+  
 end
